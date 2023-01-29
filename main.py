eb96cae3a8393e1f440c68b6aba7c5502ff97881
Nayla Hanegan <naylahanegan@gmail.com>
2023-01-29 15:23:38 -0500

    cleanup

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from urllib.request import urlopen, Request
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Adf.ly Decoder')
-    parser.add_argument('-u', '--url', type=str, metavar="url", help='Parse Adfly link and give download URL.')
+    parser.add_argument('-u', '--url', type=str, required=True, metavar="url", help='Parse Adfly link and give download URL.')
     args = parser.parse_args()
     return args
 
@@ -39,19 +39,19 @@ def decrypt(code):
                     hlp=hlp+1
                     temp=int(key[i])^int(key[y])
                     if int(temp) < 10:
-                        #print(key[i],' ',key[y]," ",temp)
                         key[i]=str(temp)
                     i=y+1
                     break
         if hlp==0:
             i=i+1
-            
     temp="".join(key)
     key=temp
     key = b64decode(key.encode("utf-8"))
     return key.decode('utf-8')
 
 def download_manifest(url):
+    if "http" not in url:
+        url = "https://" + url
     req = Request(url, headers={'User-Agent': 'Chrome/91.0.4472.77'})
     data_ = urlopen(req)
     data = data_.read()
